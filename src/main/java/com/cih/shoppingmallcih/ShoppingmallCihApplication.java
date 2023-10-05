cc88ed3a2786aa493f119280196ad6b2a9a94790
choilhyun <cihg155@gmail.com>
2023-10-05 19:20:39 +0900

    최초 커밋

---
+++ src/main/java/com/cih/shoppingmallcih/ShoppingmallCihApplication.java
@@ -0,0 +1,13 @@
+package com.cih.shoppingmallcih;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingmallCihApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingmallCihApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cih/shoppingmallcih/ShoppingmallCihApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cih.shoppingmallcih;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingmallCihApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
