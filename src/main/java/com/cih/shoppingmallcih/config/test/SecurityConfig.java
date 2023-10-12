bccfa90c8d3d262fbee087242fd9eecbadee5dca
choilhyun <cihg155@gmail.com>
2023-10-12 15:58:42 +0900

    최초 스프링 시큐리티 클래스 구성 - 엔드포인트 권한 부여 구성 재정의

---
+++ src/main/java/com/cih/shoppingmallcih/config/test/SecurityConfig.java
@@ -0,0 +1,52 @@
+package com.cih.shoppingmallcih.config.test;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Log4j2
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public UserDetailsService userDetailsService(){
+        var userDetailsService = new InMemoryUserDetailsManager();
+
+        UserDetails user = User.withUsername("cih")
+                .password("abcd1234")
+                .authorities("read")
+                .build();
+
+        userDetailsService.createUser(user);
+
+        return userDetailsService;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        log.info("configure.......");
+
+//        http.cors().and().csrf().disable()
+//                .and().authorizeRequests()
+//                .antMatchers("/home").permitAll()
+//                .antMatchers("/mypage").authenticated()
+//                .anyRequest().authenticated();
+
+        return http.build();
+    }
+
+}
