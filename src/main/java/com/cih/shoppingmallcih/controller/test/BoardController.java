24ee31c28cc9237cd7b556d21032dd4c7c87427b
choilhyun <cihg155@gmail.com>
2023-10-19 20:30:32 +0900

    목록 화면 - board list 출력

--- src/main/java/com/cih/shoppingmallcih/controller/test/BoardController.java
+++ src/main/java/com/cih/shoppingmallcih/controller/test/BoardController.java
@@ -1,7 +1,9 @@
 package com.cih.shoppingmallcih.controller.test;
 
 
+import com.cih.shoppingmallcih.dto.test.BoardDTO;
 import com.cih.shoppingmallcih.dto.test.PageRequestDTO;
+import com.cih.shoppingmallcih.dto.test.PageResponseDTO;
 import com.cih.shoppingmallcih.service.test.BoardService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -11,18 +13,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/test/board")
+@RequestMapping("/board")
 @Log4j2
 @RequiredArgsConstructor
 public class BoardController {
+
     private final BoardService boardService;
 
     @GetMapping("/list")
     public String list(PageRequestDTO pageRequestDTO, Model model){
-        log.info("board   list.......");
 
+        PageResponseDTO<BoardDTO> responseDTO = boardService.list(pageRequestDTO);
+
+        log.info(responseDTO);
+
+        model.addAttribute("responseDTO", responseDTO);
         return "/test/board/list";
-
     }
 
 }
