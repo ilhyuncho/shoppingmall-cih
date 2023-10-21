bf7dc61a38cc018eea09288f6341a445421a76e7
choilhyun <cihg155@gmail.com>
2023-10-21 14:42:46 +0900

    [Validation] 등록 처리화 화면 개발 1 @Valid

--- src/main/java/com/cih/shoppingmallcih/controller/test/BoardController.java
+++ src/main/java/com/cih/shoppingmallcih/controller/test/BoardController.java
@@ -9,8 +9,13 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/board")
@@ -29,6 +34,34 @@ public class BoardController {
 
         model.addAttribute("responseDTO", responseDTO);
         return "/test/board/list";
+    }
+
+    @GetMapping("/register")
+    public String registerGET(){
+
+        log.info("board.....register...get");
+        return "/test/board/register";
+    }
+
+    @PostMapping("/register")
+    public String registerPost(@Valid BoardDTO boardDTO, BindingResult bindingResult,
+                               RedirectAttributes redirectAttributes){
+        log.info("board POST register......");
+
+        if(bindingResult.hasErrors()){
+            log.info("has errors........");
+            redirectAttributes.addFlashAttribute("errors", bindingResult.getAllErrors());
+
+            return "redirect:/board/register";
+        }
+
+        log.info("boardDTO");
+
+        Long bno = boardService.register(boardDTO);
+
+        redirectAttributes.addFlashAttribute("result", bno);
+
+        return "redirect:/board/list";
     }
 
 }
