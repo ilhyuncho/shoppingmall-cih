e9924481cea5e1207f36c76bdfb4ef8e250a437d
choilhyun <cihg155@gmail.com>
2023-10-24 15:59:01 +0900

    [Service] 특정 게시물의 댓글 목록 처리

--- src/main/java/com/cih/shoppingmallcih/service/test/ReplyService.java
+++ src/main/java/com/cih/shoppingmallcih/service/test/ReplyService.java
@@ -1,5 +1,7 @@
 package com.cih.shoppingmallcih.service.test;
 
+import com.cih.shoppingmallcih.dto.test.PageRequestDTO;
+import com.cih.shoppingmallcih.dto.test.PageResponseDTO;
 import com.cih.shoppingmallcih.dto.test.ReplyDTO;
 
 public interface ReplyService {
@@ -7,5 +9,8 @@ public interface ReplyService {
     ReplyDTO read(Long rno);
     void modify(ReplyDTO replyDTO);
     void remove(Long rno);
+
+    // 특정 게시물의 댓글 목록
+    PageResponseDTO<ReplyDTO> getListOfBoard(Long bno, PageRequestDTO pageRequestDTO);
 }
 
--- src/main/java/com/cih/shoppingmallcih/service/test/ReplyServiceImpl.java
+++ src/main/java/com/cih/shoppingmallcih/service/test/ReplyServiceImpl.java
@@ -2,13 +2,21 @@ package com.cih.shoppingmallcih.service.test;
 
 import com.cih.shoppingmallcih.domain.test.reply.Reply;
 import com.cih.shoppingmallcih.domain.test.reply.ReplyRepository;
+import com.cih.shoppingmallcih.dto.test.PageRequestDTO;
+import com.cih.shoppingmallcih.dto.test.PageResponseDTO;
 import com.cih.shoppingmallcih.dto.test.ReplyDTO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @Log4j2
@@ -53,11 +61,32 @@ public class ReplyServiceImpl implements ReplyService {
 
     @Override
     public void remove(Long rno) {
-        Optional<Reply> byId = replyRepository.findById(rno);
 
+        // 이 부분 필요한 부분인가??
+        Optional<Reply> byId = replyRepository.findById(rno);
         if(byId.isPresent()){
+
             replyRepository.deleteById(rno);
         }
+    }
 
+    @Override
+    public PageResponseDTO<ReplyDTO> getListOfBoard(Long bno, PageRequestDTO pageRequestDTO) {
+
+        Pageable pageable = PageRequest.of(pageRequestDTO.getPage() <= 0 ? 0 : pageRequestDTO.getPage() -1,
+        pageRequestDTO.getSize(), Sort.by("rno").ascending());
+
+        Page<Reply> result = replyRepository.listOfBoard(bno, pageable);
+
+        List<ReplyDTO> dtoList = result.getContent().stream().
+                map(reply -> modelMapper.map(reply, ReplyDTO.class)).collect(Collectors.toList());
+
+        PageResponseDTO<ReplyDTO> pageResponseDTO = PageResponseDTO.<ReplyDTO>withAll()
+                .pageRequestDTO(pageRequestDTO)
+                .dtoList(dtoList)
+                .total((int)result.getTotalElements())
+                .build();
+
+        return pageResponseDTO;
     }
 }
