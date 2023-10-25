85b049798a72f201d13b275426c58b663e07d29c
choilhyun <cihg155@gmail.com>
2023-10-25 17:56:39 +0900

    [script] 댓글 페이지 번호 클릭, 조회, 수정, 삭제

--- src/main/resources/static/js/reply.js
+++ src/main/resources/static/js/reply.js
@@ -12,7 +12,6 @@ async function get1(bno){
 
     return result.data
 }
-
 // 댓글 목록 처리
 async function getList({bno, page, size, goLast}){
     const result = await axios.get(`/replies/list/${bno}`, {params: {page, size}})
@@ -28,10 +27,24 @@ async function getList({bno, page, size, goLast}){
 
     return result.data
 }
-
 // 댓글 등록
 async function addReply(replyObj){
     const response = await axios.post(`/replies/`, replyObj)
     // return 값 : {'rno':11}
+    return response.data
+}
+// 댓글 조회
+async function getReply(rno){
+    const response = await axios.get(`/replies/${rno}`)
+    return response.data
+}
+// 댓글 수정
+async function modifyReply(replyObj){
+    const response = await axios.put(`/replies/${replyObj.rno}`, replyObj)
+    return response.data
+}
+
+async function removeReply(rno){
+    const response = await axios.delete(`/replies/${rno}`)
     return response.data
 }
