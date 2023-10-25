01ef28373ecb17bd9af06f1e02cf261dd4885a78
choilhyun <cihg155@gmail.com>
2023-10-25 13:00:52 +0900

    [axios] 댓글 리스트 요청-비동기 요청 기본

---
+++ src/main/resources/static/js/reply.js
@@ -0,0 +1,14 @@
+// Axios를 이용하는 코드
+// Axios를 이용할때 async/await를 같이 이용하면
+// 비동기 처리를 동기화된  코드처럼 작성 할수 있다.
+
+
+//async : 해당 함수가 비동기 처리를 위한 함수 라는 표시
+//await : async 함수 내에서 비동기 호출하는 부분
+async function get1(bno){
+    const result = await axios.get(`/replies/list/${bno}`)
+
+    // console.log(result)
+
+    return result.data
+}
