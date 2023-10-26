a1ecb57e2551843c131a2d3efd2c9f1fa34f0589
choilhyun <cihg155@gmail.com>
2023-10-26 14:46:12 +0900

    [파일 업로드] 기초-전달된 파일 저장 (multipartFile.transferTo)

--- src/main/java/com/cih/shoppingmallcih/controller/test/UpDownController.java
+++ src/main/java/com/cih/shoppingmallcih/controller/test/UpDownController.java
@@ -10,23 +10,42 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.UUID;
+
 @RestController
 @Log4j2
 public class UpDownController {
 
     @Value("${org.zerock.upload.path}")
     private String uploadPath;
-    @ApiOperation(value="Upload POst", notes="POST 방식으로 파일 등록")
+    @ApiOperation(value="Upload post", notes="POST 방식으로 파일 등록")
     @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public String upload(UploadFileDTO uploadFileDTO){
+
         log.info(uploadFileDTO);
 
         if(uploadFileDTO.getFiles() != null){
             uploadFileDTO.getFiles().forEach(multipartFile -> {
-                log.info(multipartFile.getOriginalFilename());
+                String originalName = multipartFile.getOriginalFilename();
+
+                log.info(originalName);
+
+                String uuid = UUID.randomUUID().toString();
+
+                Path savePath = Paths.get(uploadPath, uuid+"_"+ originalName);
+
+                try{
+
+                    multipartFile.transferTo(savePath); // 실제 파일 저장
+
+                }catch(IOException e){
+                    e.printStackTrace();
+                }
             });
         }
-
         return null;
     }
 }
