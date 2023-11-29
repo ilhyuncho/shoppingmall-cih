8f56cfd4881404560d8722903ba0a86ae98e44be
choilhyun <cihg155@gmail.com>
2023-11-29 12:57:13 +0900

    [QueryDSL] QueryDSLConfig를 활용한 구성, JPAQueryFactory객체를 @Bean객체로 등록

---
+++ src/test/java/com/cih/shoppingmallcih/repository/test/QueryDSLConfigProductRepositoryTest.java
@@ -0,0 +1,34 @@
+package com.cih.shoppingmallcih.repository.test;
+
+import com.cih.shoppingmallcih.domain.test.product.QProduct;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+public class QueryDSLConfigProductRepositoryTest {
+
+    // Bean 객체로 등록된 JPAQueryFactory를 활용
+
+    @Autowired
+    JPAQueryFactory jpaQueryFactory;
+
+    @Test
+    void queryDslTest4(){
+        QProduct qProduct = QProduct.product;
+
+        List<String> listResult = jpaQueryFactory.select(qProduct.name)
+                .from(qProduct)
+                .where(qProduct.name.eq("펜2"))
+                .orderBy(qProduct.price.asc())
+                .fetch();
+
+        for (String s : listResult) {
+            System.out.println(s);
+        }
+    }
+
+}
