d0a8cbe93f42293f5da0bad687a62099852c0273
choilhyun <cihg155@gmail.com>
2024-01-17 17:55:55 +0900

    [워크북690]-시큐리티- 어노테이션을 이용한 권한 체크

--- src/main/java/com/cih/shoppingmallcih/config/test/security/CustomSecurityConfig.java
+++ src/main/java/com/cih/shoppingmallcih/config/test/security/CustomSecurityConfig.java
@@ -5,6 +5,7 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -14,13 +15,18 @@ import org.springframework.security.web.SecurityFilterChain;
 @Log4j2
 @Configuration
 @RequiredArgsConstructor
+//@EnableGlobalMethodSecurity(prePostEnabled = true)  // 어노테이션을 이용한 권한 체크
+@EnableMethodSecurity(securedEnabled = true, prePostEnabled = true)
+// securedEnabled => Secured 애노테이션 사용 여부, prePostEnabled => PreAuthorize 어노테이션 사용 여부.
 public class CustomSecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         log.error("=================configue");
 
-        http.formLogin();
+        http.formLogin().loginPage("/member/login");
+
+        http.csrf().disable();
 
         return http.build();
     }
--- src/main/java/com/cih/shoppingmallcih/controller/test/BoardController.java
+++ src/main/java/com/cih/shoppingmallcih/controller/test/BoardController.java
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -63,6 +64,7 @@ public class BoardController {
         return "/test/board/list";
     }
 
+    @PreAuthorize("hasRole('USER')")
     @GetMapping("/register")
     public String registerGET(){
 
