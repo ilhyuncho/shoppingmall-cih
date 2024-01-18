87e0295ffb5190f34211e26f486041d1032a5e8c
choilhyun <cihg155@gmail.com>
2024-01-18 17:41:54 +0900

    [워크북728]- 회원 데이터 처리 - MemberSecurityDTO 로 로그인 처리 ( SimpleGrantedAuthority )

--- src/main/java/com/cih/shoppingmallcih/config/test/security/CustomUserDetailService.java
+++ src/main/java/com/cih/shoppingmallcih/config/test/security/CustomUserDetailService.java
@@ -1,13 +1,19 @@
 package com.cih.shoppingmallcih.config.test.security;
 
+import com.cih.shoppingmallcih.domain.member.Member;
+import com.cih.shoppingmallcih.domain.member.MemberRepository;
+import com.cih.shoppingmallcih.dto.member.MemberSecurityDTO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Log4j2
 @Service
@@ -15,14 +21,35 @@ import org.springframework.stereotype.Service;
 public class CustomUserDetailService implements UserDetailsService {
 
     private final PasswordEncoder passwordEncoder;
+    private final MemberRepository memberRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        UserDetails userDetails = User.builder().username("user1")
-                .password(passwordEncoder.encode("1111"))
-                .authorities("ROLE_USER")
-                .build();
 
-        return userDetails;
+//        UserDetails userDetails = User.builder().username("user1")
+//                .password(passwordEncoder.encode("1111"))
+//                .authorities("ROLE_USER")
+//                .build();
+//
+//        return userDetails;
+
+        log.info("loadUserByUsername: " + username);
+
+        Optional<Member> result = memberRepository.getWithRoles(username);
+        if(result.isEmpty()){
+            throw new UsernameNotFoundException("username not foudn.........");
+        }
+
+        Member member = result.get();
+
+        MemberSecurityDTO memberSecurityDTO= new MemberSecurityDTO(member.getMid(),
+            member.getMpw(),
+            member.getEmail(),
+            member.isDel(),
+            false,
+            member.getRoleSet().stream().map(memberRole -> new SimpleGrantedAuthority("ROLE_"+memberRole.name()))
+                    .collect(Collectors.toList()));
+            log.info("memberSecurityDTO: " + memberSecurityDTO );
+            return memberSecurityDTO;
     }
 }
