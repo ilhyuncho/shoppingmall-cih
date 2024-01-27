1b6d21b418548ee32724ed55ee404a3c4a386ef4
choilhyun <cihg155@gmail.com>
2024-01-27 14:51:27 +0900

    [핵심364]- RestTemplate 커스텀 설정(httpClient) - 커넥션 풀 기능 활성화

--- src/main/java/com/cih/shoppingmallcih/restTemplateSample/roleClient/service/RestTemplateService.java
+++ src/main/java/com/cih/shoppingmallcih/restTemplateSample/roleClient/service/RestTemplateService.java
@@ -2,8 +2,13 @@ package com.cih.shoppingmallcih.restTemplateSample.roleClient.service;
 
 import com.cih.shoppingmallcih.controller.restTemplateForServer.MemberDTO;
 import lombok.extern.log4j.Log4j2;
+import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.HttpClients;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -100,11 +105,37 @@ public class RestTemplateService {
                 .header("my-header", "kwidj api")
                 .body(memberDTO);
 
-        RestTemplate restTemplate = new RestTemplate();
+        //RestTemplate restTemplate = new RestTemplate();
+
+        // 커스텀 RestTemplate 사용 하기
+        RestTemplate restTemplate = restTemplate();
+
         ResponseEntity<MemberDTO> responseEntity = restTemplate.exchange(requestEntity, MemberDTO.class);
 
         return responseEntity;
     }
 
+    public RestTemplate restTemplate(){
+        // 주 목적 : 커넥션 풀 기능 활성화
+        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+
+        HttpClient client = HttpClientBuilder.create()
+                .setMaxConnTotal(500)       //최대 오픈되는 커넥션 수
+                .setMaxConnPerRoute(500)    //IP, 포트 1쌍에 대해 수행할 커넥션 수
+                .build();
+
+        CloseableHttpClient httpClient = HttpClients.custom()
+                .setMaxConnTotal(500)
+                .setMaxConnPerRoute(500)
+                .build();
+
+        factory.setHttpClient(httpClient);
+        factory.setConnectTimeout(2000);       //연결시간초과, ms
+        factory.setReadTimeout(5000);          //읽기시간초과, ms
+
+        RestTemplate restTemplate = new RestTemplate(factory);
+
+        return restTemplate;
+    }
 
 }
